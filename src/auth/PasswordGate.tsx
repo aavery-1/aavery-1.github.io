63c6678941864492d073364b73267b7021754f48
aavery-1 <aavery@apps.teamschools.org>
2026-09-11 13:59:57 -0400

    Update PasswordGate.tsx

--- src/auth/PasswordGate.tsx
+++ src/auth/PasswordGate.tsx
@@ -56,6 +56,7 @@ export function PasswordGate({ children }: { children: ReactNode }) {
           id="pwgate-password"
           className="pwgate__input"
           labelText="Password"
+          hideLabel
           placeholder="Enter password"
           value={pw}
           invalid={invalid}
